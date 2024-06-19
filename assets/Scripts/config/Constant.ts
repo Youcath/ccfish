5fa2ae137f9695ec47fd84069e183221dba73a82
Yang Tao Roy <501431767@qq.com>
2024-06-19 22:20:47 +0800

    0619 2220

---
+++ assets/Scripts/config/Constant.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('Constant')
+export class Constant extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
